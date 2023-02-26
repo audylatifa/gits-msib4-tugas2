dfd34c45a97cf5faf6a0cb710912d72ceedaac7b
audylatifa <125886431+audylatifa@users.noreply.github.com>
2023-02-26 12:47:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>websiteaudylatifa</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+          
+            <div class="logo"><a href=''>audylatifa</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href ="#Home">Home</a></li>
+                    <li><a href ="#Profile">Profile</a></li>
+                    <li><a href ="#Contact">Contact</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+
+    </section>
+     <!--untuk home-->
+        <section id="Home">
+            <img src="https://img.freepik.com/free-vector/colourful-illustration-programmer-working_23-2148281410.jpg?size=338&ext=jpg&ga=GA1.2.391025562.1677137913&semt=sph"/>
+            <div class="kolom">
+                <p class="deskripsi">Studi Independent 2023</p>
+                <h2>Full Stack Web Developer Gits</h2>
+                <p>The real threat is actually not when the computer begins to think like a human, but when humans begin to think like computers.</p>
+
+            </section>
+          >
+            <!--untuk profile-->
+            <section id="profile">
+                <div class="tengah">
+                    <div class="kolom">
+                        <h2>My Profile</h2>
+                    </div>
+
+                    <div class="profile">
+                        <div class="kartu-profile">
+
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHn8-A2IEH1uIa1DNVb-gswOzJNmSAW7e68A&usqp=CAU"/>
+                            <p>Hii Friends! I would like to introduce myself to you. My name is Audy Latifa Yasmin and people call me audy.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+          </div>
+             <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                        <h3>About</h3>
+                        <p>I'm a student at the Mataram University of Technology. I'm currently in my 6th semester. This year I took part in independent studies and was accepted at Gits Web Developer.</p>
+                    </div>
+                     <div class="footer-section">
+                        <h3>Contact</h3>
+                        <p><b>gmail: </b>audylatifa442@gmail.com</p>
+                    </div>
+                      <div class="footer-section">
+                        <h3>Social</h3>
+                        <p><b>Instagram: </b>audylatifa_</p>
+                    </div>
+                </div>
+             </div>
+       </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>audylatifa</b> All  Rights Reserved.
+        </div>
+    </div>
+</body>
+</html>
